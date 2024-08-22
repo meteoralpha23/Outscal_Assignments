c0a2dfe707827d6ac2a72b18a8eac80fa8c51332
meteoralpha23 <61462391+meteoralpha23@users.noreply.github.com>
2024-08-22 21:26:25 +0530

    Create InvertedStarPatternusingLoops.cpp

---
+++ InvertedStarPatternusingLoops.cpp
@@ -0,0 +1,16 @@
+
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+    for(int i=6; i>=0; i--)
+    {
+        for(int j=i; j>=0;j--)
+        {
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+}
